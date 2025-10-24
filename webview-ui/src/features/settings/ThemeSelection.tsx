65238cb4fca668baeb6d95a3b5cf04af649eeb25
Sajjad Zare <s.zarepour@neshan.org>
2025-10-24 11:05:48 +0330

    chore: comment out ThemeSelection component and its imports for future reference

--- webview-ui/src/features/settings/ThemeSelection.tsx
+++ webview-ui/src/features/settings/ThemeSelection.tsx
@@ -1,32 +1,32 @@
-import { THEME_OPTIONS } from "@/constants/constants";
-import React from "react";
+// import { THEME_OPTIONS } from "@/constants/constants";
+// import React from "react";
 
-const ThemeSelection = ({
-  selectedTheme,
-  onSelectedTheme,
-}: {
-  selectedTheme: string | null;
-  onSelectedTheme: (value: string) => void;
-}) => {
-  return (
-    <div className="flex flex-col mb-8 space-y-2">
-      <label htmlFor="theme-select" className="font-medium text-base">
-        Theme
-      </label>
-      <select
-        id="theme-select"
-        value={selectedTheme || "vscode"}
-        onChange={(e) => onSelectedTheme(e.target.value)}
-        className="border-1 border-white-text/30 px-1 py-2 bg-gray-secondry rounded-md"
-      >
-        {THEME_OPTIONS.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.name}
-          </option>
-        ))}
-      </select>
-    </div>
-  );
-};
+// const ThemeSelection = ({
+//   selectedTheme,
+//   onSelectedTheme,
+// }: {
+//   selectedTheme: string | null;
+//   onSelectedTheme: (value: string) => void;
+// }) => {
+//   return (
+//     <div className="flex flex-col mb-8 space-y-2">
+//       <label htmlFor="theme-select" className="font-medium text-base">
+//         Theme
+//       </label>
+//       <select
+//         id="theme-select"
+//         value={selectedTheme || "vscode"}
+//         onChange={(e) => onSelectedTheme(e.target.value)}
+//         className="border-1 border-white-text/30 px-1 py-2 bg-gray-secondry rounded-md"
+//       >
+//         {THEME_OPTIONS.map((option) => (
+//           <option key={option.value} value={option.value}>
+//             {option.name}
+//           </option>
+//         ))}
+//       </select>
+//     </div>
+//   );
+// };
 
-export default ThemeSelection;
+// export default ThemeSelection;
